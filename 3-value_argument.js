06ae20d0a938d822d682d95415301bdb36f7c254
Aya Elsayed <drayaelsayed22@gmail.com>
2025-12-01 22:48:24 +0200

    fix argument handling logic in 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
-if (args.length === 0) {
-    console.log('No argument');
+if (args[0]) {
+    console.log(args[0]);
 }
 else {
-    console.log(args[0]);
+    console.log('No argument');
 }
